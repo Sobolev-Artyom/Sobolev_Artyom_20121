4aa1c291fe2acdedc4f3811d6b6104fc66ae9c9f
Sobolev-Artyom <115024655+Sobolev-Artyom@users.noreply.github.com>
2022-10-22 19:33:54 +0300

    Update task 4_1.py

--- Задачи 4/task 4_1.py
+++ Задачи 4/task 4_1.py
@@ -1,15 +1,18 @@
+def main()
+   val = input()
+   num_list = list(map(int, val))
+   is_polindrom = True
 
-val = input()
-num_list = list(map(int, val))
-is_polindrom = True
+   size = int(len(num_list))
+   for i in range(0, int(size / 2)):
+       if num_list[i] != num_list[size - i-1]:
+           is_polindrom = False
+           break
 
-size = int(len(num_list))
-for i in range(0, int(size / 2)):
-    if num_list[i] != num_list[size - i-1]:
-        is_polindrom = False
-        break
+   if is_polindrom:
+       print("True")
+   else:
+       print("False")
 
-if is_polindrom:
-    print("True")
-else:
-    print("False")
+if __name__ =='__main__':
+    main()
