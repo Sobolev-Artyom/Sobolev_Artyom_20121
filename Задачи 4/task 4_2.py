f0340fdfb6fe5b233d06a92297b622ff1cf7b10e
Sobolev-Artyom <115024655+Sobolev-Artyom@users.noreply.github.com>
2022-10-23 08:34:23 +0300

    Update task 4_2.py

--- Задачи 4/task 4_2.py
+++ Задачи 4/task 4_2.py
@@ -1,15 +1,16 @@
-CHAR_MIN = -2**7
-CHAR_MAX = 2**7 - 1
+def main()
+   CHAR_MIN = -2**7
+   CHAR_MAX = 2**7 - 1
 
-val = int(input())
+   val = int(input())
 
-num_list = list(map(int, str(abs(val))))
-num_list.reverse()
-rev_val = int(''.join(map(str,num_list)))
-if val < 0:
-    rev_val *= -1
+   num_list = list(map(int, str(abs(val))))
+   num_list.reverse()
+   rev_val = int(''.join(map(str,num_list)))
+   if val < 0:
+      rev_val *= -1
 
-if rev_val < CHAR_MIN or CHAR_MAX < rev_val:
-    print("no solution")
-else:
-    print(rev_val)
+   if rev_val < CHAR_MIN or CHAR_MAX < rev_val:
+       print("no solution")
+   else:
+       print(rev_val)
